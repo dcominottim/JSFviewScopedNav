30b9d09f3d2792b2e93f72bcc184b5385bb290c1
Darren Kelly <webelcomau@users.noreply.github.com>
2016-11-12 22:57:29 +1100

    Preparation for public archive download

--- src/java/com/webel/jsf/JavaUtil.java
+++ src/java/com/webel/jsf/JavaUtil.java
@@ -1,24 +1,19 @@
-/*
- * (C) Copyright 2016 Greensoft Pty Ltd, Australia (ACN 148 917 999). All rights reserved.
- */
 package com.webel.jsf;
 
 import java.io.Serializable;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
-//import javax.enterprise.context.Dependent;
 
 /**
  *
  * @author darrenkelly
  */
 @Named(value = "javaUtil")
-//@Dependent
 @ApplicationScoped
 public class JavaUtil implements Serializable {
 
     /**
-     * Creates a new instance of JavaHelper
+     * Creates a new instance.
      */
     public JavaUtil() {
     }
