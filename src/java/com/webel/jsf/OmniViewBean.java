c445aa6d15223582dee6f47e3e3472a24c84c9db
Darren Kelly (Webel IT Australia) <webelcomau@users.noreply.github.com>
2016-11-12 13:00:01 +1100

    Version with OmniFaces variation page, and choose bean form from home page.
    Compare com.sun.faces.numberOfLogicalViews and org.omnifaces.VIEW_SCOPE_MANAGER_MAX_ACTIVE_VIEW_SCOPES
    
    AFAIKT neither respected alone for: omnifaces-2.5.1.

---
+++ src/java/com/webel/jsf/OmniViewBean.java
@@ -0,0 +1,22 @@
+package com.webel.jsf;
+
+import javax.inject.Named;
+import org.omnifaces.cdi.ViewScoped;
+
+/**
+ *
+ * Visit <a href="http://showcase.omnifaces.org/cdi/ViewScoped">OmniFaces @ViewScoped</a>.
+ * 
+ * @author darrenkelly
+ */
+@Named
+@ViewScoped
+public class OmniViewBean extends AbstractViewBean {
+
+    /**
+     * Creates a new instance.
+     */
+    public OmniViewBean() {
+    }
+    
+}
